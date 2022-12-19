1761bc4c739ec750284e13047d5708bfd58aef38
Philip Linde <philip@text.garden>
2022-12-19 12:02:37 +0100

    Implement proper sine-law panning

--- panner.c
+++ panner.c
@@ -5,9 +5,9 @@
 
 void panner_tick(struct panner *p)
 {
-	const value_t in = (p->bus_pan + (2.0 * p->params.pan - 1.0));
-	p->out_left = 0.5 * (1.0 + sin(0.5 * M_PI * in));
-	p->out_right = 0.5 * (1.0 - sin(0.5 * M_PI * in));
+	const value_t in = p->bus_pan + p->params.pan;
+	p->out_left = sin(0.5 * M_PI * in);
+	p->out_right = sin(0.5 * M_PI * (1.0 - in));
 }
 
 void panner_reset(struct panner *p)
